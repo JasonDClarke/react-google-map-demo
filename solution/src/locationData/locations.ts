2db3f7c5c9e2cac07329d31fbf2d88c71e9a4ab7
Jason Clarke <jasondanielclarke@gmail.com>
2024-10-16 09:54:42 +0100

    move locations to file

---
+++ solution/src/locationData/locations.ts
@@ -0,0 +1,24 @@
+import { Poi } from "./types";
+
+export const locations: Poi[] = [
+    {key: 'bigBen', location: { lat: 51.5007292, lng: -0.1246254 }},
+  {key: 'londonEye', location: { lat: 51.503324, lng: -0.119543 }},
+  {key: 'britishMuseum', location: { lat: 51.519413, lng: -0.126956 }},
+  {key: 'towerOfLondon', location: { lat: 51.5081124, lng: -0.0759493 }},
+  {key: 'buckinghamPalace', location: { lat: 51.501364, lng: -0.14189 }},
+  {key: 'hydePark', location: { lat: 51.5072682, lng: -0.1657303 }},
+  {key: 'edinburghCastle', location: { lat: 55.9485941, lng: -3.199913 }},
+  {key: 'stonehenge', location: { lat: 51.178882, lng: -1.826215 }},
+  {key: 'windsorCastle', location: { lat: 51.4838917, lng: -0.6043887 }},
+  {key: 'yorkMinster', location: { lat: 53.9612043, lng: -1.0812766 }},
+  {key: 'liverpoolCathedral', location: { lat: 53.3975959, lng: -2.973455 }},
+  {key: 'cardiffCastle', location: { lat: 51.481667, lng: -3.181944 }},
+  {key: 'giantCauseway', location: { lat: 55.240807, lng: -6.511573 }},
+  {key: 'nationalGallery', location: { lat: 51.5089292, lng: -0.128299 }},
+  {key: 'oxfordUniversity', location: { lat: 51.7548163, lng: -1.254367 }},
+  {key: 'lochNess', location: { lat: 57.322857, lng: -4.424382 }},
+  {key: 'hadrainsWall', location: { lat: 55.015927, lng: -2.361737 }},
+  {key: 'greenwichObservatory', location: { lat: 51.476847, lng: -0.000500 }},
+  {key: 'stPaulsCathedral', location: { lat: 51.5138455, lng: -0.0983506 }},
+  {key: 'bristolSuspensionBridge', location: { lat: 51.4549091, lng: -2.6266471 }},
+  ];
