304b6b2dacb304915c0e904458ecb6f8e28945bc
Jason Clarke <jasondanielclarke@gmail.com>
2024-10-21 16:37:21 +0100

    amend to only include demo data (safe address data)

--- src/components/church-data.tsx
+++ src/components/church-data.tsx
@@ -36,23 +36,13 @@ export const ChurchData = ({
 
                 return (
                     <div key={location.key}>
-                        <h2>{location['Church or Organisation']}</h2>
-                        <h3>{location['Course start date']}</h3>
+                        <h2>{location['Name']}</h2>
 
                         <p>Distance: {distance.toFixed(1)} miles</p>
 
-                        <p>{location['Church Address']}</p>
-                        <p>{location['Church Postcode / ZIP Code']}</p>
-                        <p>{location['Location / City']}</p>
-                        <p>{location['Church Country']}</p>
-
-                        <p>Contact:</p>
-                        <p>{location['First & Last Name']}</p>
-                        <p>{location['Church Contact number / email']}</p>
-                        <p>{location['Church Website URL']}</p>
-
-                        <p>Notes:</p>
-                        <p>{location.Notes}</p>
+                        <p>{location['Address']}</p>
+                        <p>{location['ZipCode']}</p>
+                        <p>{location['Country']}</p>
                     </div>
                 )
             })}
